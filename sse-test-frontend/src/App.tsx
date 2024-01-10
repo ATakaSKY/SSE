6ce94ec534f5b4251f076d6ae0a38b453a88f7d1
Aakash Thakur <thakurakash321@gmail.com>
2024-01-10 22:40:16 +0530

    added log streaming (#1)

--- sse-test-frontend/src/App.tsx
+++ sse-test-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   useEffect(() => {
-    const eventSource = new EventSource("http://localhost:3000/stream");
+    const eventSource = new EventSource("http://localhost:3000/stream2");
     eventSource.onmessage = ({ data }) => {
       console.log("New message", JSON.parse(data));
     };
